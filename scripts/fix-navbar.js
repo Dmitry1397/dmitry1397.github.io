6a870618980754eced7db2219977e4c832b3b608
Dmitry1397 <dyshik8@gmail.com>
2022-07-05 13:55:54 +0200

    change stile for mobile

--- scripts/fix-navbar.js
+++ scripts/fix-navbar.js
@@ -15,3 +15,16 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+/*
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+*/
